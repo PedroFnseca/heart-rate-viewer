29d06986c838cfb59ba994ea215cc39f18d02af6
pedrofnseca <pedropereira.pp264@gmail.com>
2023-04-30 17:50:11 -0300

    :lipstick: feat: show label only desktop

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,10 +1,8 @@
 import { AiOutlineLogout } from "react-icons/ai";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 export default function LogoutButton() {
   const router = useRouter();
-  const [isHovered, setIsHovered] = useState(false);
 
   function onClick() {
     sessionStorage.removeItem("token");
@@ -12,25 +10,19 @@ export default function LogoutButton() {
     router.push("/auth");
   }
 
-  function handleHover() {
-    setIsHovered(true);
-  }
-
-  function handleLeave() {
-    setIsHovered(false);
+  function getSizeWindow() {
+    return window.innerWidth;
   }
 
   return (
     <button
       className="bg-blue-500 text-white font-bold py-2 px-4 rounded flex items-center hover:bg-blue-700 transition duration-200"
       onClick={onClick}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleLeave}
     >
       <span className="mr-1">
         <AiOutlineLogout />
       </span>
-      {isHovered ? "Desconectar" : "Sair"}
+      { getSizeWindow() > 640 ? "Sair" : "" }
     </button>
   );
 }
